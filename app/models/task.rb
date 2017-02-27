474f71e9448a1324597a6f45175be887bac6c9b6
neha <neha@pstakecare.com>
2017-02-27 15:19:40 +0530

    Add filter logic with scopes to models

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,8 @@ class Task < ApplicationRecord
   # TODO - if time permits, try to do this.
   validates_date :end_date_on
 
-  scope :author,       -> (user_id) { where('user_id = ?', user_id) }
-  # TODO - Does having updated_at here make sense?
-  scope :tasks_after_date, -> (created_after) { order('created_at >= ?', created_after) }
+  scope :author,       -> (user_id) { where("user_id = ?", user_id) }
+  # TODO - Why records of same date are returned.
+  scope :tasks_after_date, -> (created_after) { where("created_at > ?", created_after) }
   #scope :before_end_date,  -> { order('end_date_on <= ?', :date) }
 end
