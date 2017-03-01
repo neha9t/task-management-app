3bd9519797de6a20c68aa4b0b33f2270efb0ec51
neha <neha@pstakecare.com>
2017-03-01 14:51:44 +0530

    Add route for autocomplete

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   apipie
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
-  # post '/tasks', to: 'tasks#create'
-  # get '/tasks', to: 'tasks#index'
-  # get '/tasks/:id', to: 'tasks#show'
-  # put '/tasks/:id', to: 'tasks#update'
-  # delete '/tasks/:id', to: 'tasks#destroy'
+  get '/autocomplete', to: 'tasks#autocomplete'
 end
