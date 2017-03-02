09a9a1a95b53606e94b5bf6671466960f6abe00f
neha <neha@pstakecare.com>
2017-03-02 13:27:20 +0530

    Add protect from forgery with exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def record_not_found
