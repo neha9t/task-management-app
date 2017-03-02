4d2cde070a3d4575ad724054d68389b4ea43a81b
neha <neha@pstakecare.com>
2017-03-02 12:23:46 +0530

    Remove unused methods

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -1,22 +1,10 @@
 class String
 
-  def matching(string)
-    remove_special_chars.singularize_spaces == string.remove_special_chars.singularize_spaces ? self : nil
-  end
-
   def remove_special_chars
-    downcase.gsub(/[^0-9A-Za-z*\s]/, '')
-  end
-
-  def replace_space_with_dash
-    gsub(/\s/, '-')
+    gsub(/[^0-9A-Za-z\s]/, '')
   end
 
   def singularize_spaces
     gsub(/\s+/, ' ')
-  end
-
-  def sql_sanity
-    gsub(/[^0-9a-z &]/i, ' ').squish
   end
 end
